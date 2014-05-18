12fe9d43098008046bca1b986eb1a1238d4ce59c
Yorrick <yorrick.jansen@gmail.com>
2014-05-18 08:35:42 +0200

    started writing sms service for twilio

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,46 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.Logger
+import play.libs.Akka
+import akka.actor._
+
+
+case class Sms(val from: String, val content: String)
+case class StoreSms(val sms: Sms)
+
+
+class SmsStorage extends Actor {
+
+  private var smsList = List[Sms]()
+
+  def receive = {
+    case StoreSms(sms) =>
+      Logger.debug(s"Storing this sms: $sms")
+      smsList = smsList :+ sms
+  }
+}
+
 
 object Application extends Controller {
+
   def index = Action {
     Ok(views.html.index("Hello Play Framework"))
   }
+
+  // TODO write a body parser
+  def sms = Action { request =>
+  	Logger.debug(s"Got this request ${request}")
+  	Logger.debug(s"Got those data ${request.body}")
+  	
+  	// TODO build sms object with a Form to check data validity
+  	val sms = Sms("06123456789", "Toto")
+  	
+	val smsStorage = Akka.system.actorOf(Props[SmsStorage])
+	smsStorage ! StoreSms(sms)
+
+	// TODO answer with TwiML instead
+    Ok("Hello there!")
+  }
+
 }
--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   "org.webjars" %% "webjars-play" % "2.2.0", 
   "org.webjars" % "bootstrap" % "3.1.1",
-  "org.webjars" % "jquery" % "2.1.1"
+  "org.webjars" % "jquery" % "2.1.1",
+  "com.twilio.sdk" % "twilio-java-sdk" % "3.3.16"
 )
 
 playScalaSettings
