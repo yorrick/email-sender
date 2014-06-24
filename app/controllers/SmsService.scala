306bee4cfadc5e00c6a57eb0c9dbad39d3276838
Yorrick Jansen <yjansen@auto123.com>
2014-06-24 13:51:01 -0400

    tests refactoring

--- app/controllers/SmsService.scala
+++ app/controllers/SmsService.scala
@@ -1,5 +1,9 @@
 package controllers
 
+import java.io.Writer
+
+import reactivemongo.core.commands.LastError
+
 import scala.concurrent.duration._
 import scala.concurrent.Future
 
@@ -11,16 +15,14 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.Play.current
 import play.api.libs.json._
 
-import play.libs.Akka
-
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import play.modules.reactivemongo.json.collection.JSONCollection
 
-import akka.actor._
-import akka.pattern.ask
 import akka.util.Timeout
 
 import reactivemongo.api._
+
+import scala.util.{Failure, Success}
 
 
 // TODO add creation date
@@ -41,13 +43,13 @@ object SmsStorage {
   def db: reactivemongo.api.DB = ReactiveMongoPlugin.db
   def collection: JSONCollection = db.collection[JSONCollection]("smslist")
 
-  def storeSms(sms: Sms) {
+  def storeSms(sms: Sms): Future[LastError] = {
     import JsonFormats._
     Logger.debug(s"Storing this sms: $sms")
     collection.insert(sms)
   }
 
-  def listSms() = {
+  def listSms(): Future[List[Sms]] = {
     import JsonFormats._
     // let's do our query
     val cursor: Cursor[Sms] = collection.
@@ -82,30 +84,44 @@ object SmsService extends Controller {
   	import scala.language.postfixOps
   	implicit val timeout = Timeout(1 second) 
   	
-    val futureSmsList = SmsStorage.listSms().mapTo[List[Sms]] recover {
-      case error @ _ =>
-        Logger.debug(s"Could not get sms list: $error")
-        List[Sms]()
-    }
+    val futureSmsList = SmsStorage.listSms().mapTo[List[Sms]]
 
-    futureSmsList.map(smsList => Ok(views.html.sms.list(smsList)))
+    futureSmsList.map(smsList => Ok(views.html.sms.list(smsList))) recover {
+      case error @ _ =>
+        val message = s"Could not get sms list: $error"
+        Logger.warn(message)
+        NotFound(message)
+    }
   }
 
   // POST for twilio when we receive an SMS
-  def receive = Action { implicit request =>
-	smsForm.bindFromRequest.fold(
-	  formWithErrors => {
-	  	val message = s"Could not bind the form: ${formWithErrors}"
-	  	Logger.debug(message)
-	    BadRequest(message)
-	  },
-	  sms => {
-	  	Logger.debug(s"Built sms object $sms")
-		  SmsStorage.storeSms(sms)
+  def receive = Action.async { implicit request =>
 
-    	Ok(emptyTwiMLResponse)
-	  }
-	)
+    smsForm.bindFromRequest.fold(
+      formWithErrors => handleFormError(formWithErrors),
+      sms => handleFormValidated(sms)
+    )
+  }
+
+  private def handleFormError(formWithErrors: Form[Sms]) = {
+    val message = s"Could not bind the form: ${formWithErrors}"
+    Logger.warn(message)
+    Future {
+      BadRequest(message)
+    }
+  }
+
+  private def handleFormValidated(sms: Sms) = {
+    Logger.debug(s"Built sms object $sms")
+    SmsStorage.storeSms(sms).mapTo[LastError] map { lastError =>
+      if (lastError.inError == true) {
+        val message = s"Could not save the sms: ${lastError.message}"
+        Logger.warn(message)
+        BadRequest(message)
+      } else {
+        Ok(emptyTwiMLResponse)
+      }
+    }
   }
 
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,46 +1,30 @@
-import java.util.concurrent.Future
-import java.util.logging.Logger
+import controllers.{JsonFormats, Sms}
+
+import java.util.logging
 
 import com.github.athieriot.{EmbedConnection, CleanAfterExample}
-import controllers.{JsonFormats, Sms}
 import de.flapdoodle.embed.mongo.{Command, MongodStarter}
 import de.flapdoodle.embed.mongo.config.{RuntimeConfigBuilder, MongodConfigBuilder}
 import de.flapdoodle.embed.mongo.distribution.Version
 import de.flapdoodle.embed.process.config.io.ProcessOutput
+import org.junit.runner.RunWith
 import org.specs2.execute.{Result, AsResult}
 import org.specs2.mutable._
 import org.specs2.runner._
-import org.junit.runner._
-import org.specs2.specification._
-import org.specs2.specification
-import org.specs2.mutable
+import org.specs2.specification.{Scope, AfterExample}
 
-import play.api.libs.json.Json
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.{Format, Json}
+import play.api.{Application, Logger}
 import play.api.test._
 import play.api.test.Helpers._
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.api.Cursor
+import reactivemongo.bson.BSONDocument
+import reactivemongo.core.commands.LastError
 
+import scala.concurrent.{Future, Await}
 
-/**
- * Provides a connection and a collection to a test
- */
-trait InitDB {
-  self: WithApplication =>
-
-    val collectionName: String
-    def db: reactivemongo.api.DB = ReactiveMongoPlugin.db
-    def collection: JSONCollection = db.collection[JSONCollection](collectionName)
-
-    override def around[T: AsResult](t: => T): Result = {
-      before
-      Helpers.running(app)(AsResult.effectively(t))
-    }
-
-    def before: Any
-
-}
 
 /**
  * Remove all logs from embed mongodb
@@ -50,7 +34,7 @@ trait QuietEmbedConnection extends EmbedConnection {
 
   override def embedMongoDBVersion(): Version.Main = { Version.Main.V2_6 }
 
-  val logger = Logger.getLogger(getClass().getName());
+  val logger = logging.Logger.getLogger(getClass().getName());
 
   // removes all logging from embedmongo
   lazy val  runtimeConfig = new RuntimeConfigBuilder()
@@ -60,124 +44,209 @@ trait QuietEmbedConnection extends EmbedConnection {
   override lazy val runtime =  MongodStarter.getInstance(runtimeConfig)
 }
 
+//
+///**
+// * Provides a connection and a collection to a test
+// */
+//trait InitDB { self: WithApplication =>
+//
+//  val collectionName: String
+//  def db: reactivemongo.api.DB = ReactiveMongoPlugin.db
+//  def collection: JSONCollection = db.collection[JSONCollection](collectionName)
+//
+////  override def around[T: AsResult](t: => T): Result = {
+////    val newBlock = { () =>
+////      println("Executing before")
+////      before
+////      println("Executed before")
+////      t
+////    }
+////    Helpers.running(app)(AsResult.effectively(newBlock))
+////  }
+////
+////  def before: Any
+//
+//}
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-@RunWith(classOf[JUnitRunner])
-class SmsSpec extends Specification with QuietEmbedConnection with PlaySpecification {
-  sequential
-//  isolated
+trait DBInitializer {
+  def setup: Application => Unit = _ => Unit
+  def teardown: Application => Unit = _ => Unit
+}
 
-  step {
-    println("Before class")
-  }
 
-  trait SmsInitDB extends InitDB { self: WithApplication =>
-    val collectionName = "smsList"
-    def before = println("Should initialize DB here")
-  }
+abstract class WithEnv(val app: FakeApplication = FakeApplication(),
+                       var setup: Application => Unit = _ => Unit,
+                       teardown: Application => Unit = _ => Unit) extends Around with Scope {
 
-  "Sms module" should {
+  implicit def implicitApp = app
 
-    "Accept post data for sms" in new WithApplication with SmsInitDB {
-
-      val request = FakeRequest(POST, "/sms/").withFormUrlEncodedBody(
-        "To" -> "666666666",
-        "From" -> "77777777",
-        "Body" -> "hello toto"
-      )
-
-      val response = controllers.SmsService.receive()(request)
-
-      status(response) must equalTo(OK)
-      contentAsString(response) must contain("Message")
-
-      import JsonFormats._
-      val cursor: Cursor[Sms] = collection.
-        // find all sms
-        find(Json.obj()).
-        // perform the query and get a cursor of JsObject
-        cursor[Sms]
-//      val future : Future[List[Sms]] = cursor.collect[List]()
-      val future = cursor.collect[List]()
-      val result = await(future)
-
-      println(db)
-      println(s"TOTO: $result")
-    }
-
-  }
-
-  "The 'Hello world' string" should {
-    "contain 11 characters" in {
-      running(FakeApplication()) {
-        println("contain 11 characters")
-        "Hello world" must have size (11)
+  override def around[T: AsResult](t: => T): Result = {
+    Helpers.running(app)(AsResult.effectively {
+      setup(app)
+      try {
+        t
+      } finally {
+        teardown(app)
       }
-    }
-  }
-//    "start with 'Hello'" in {
-//      println("start with hello")
-//      "Hello world" must startWith("Hello")
-//    }
-//  }
-
-  step {
-    println("After class")
+    })
   }
 }
 
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-@RunWith(classOf[JUnitRunner])
-//class ApplicationSpec extends Specification with BeforeAfter { sequential
-class ApplicationSpec extends Specification { sequential
+class InitDB[T](val data: Tuple3[String, List[T], Format[T]]*) extends Around with Scope {
 
-  "Application" should {
+  import scala.concurrent.duration._
+  import play.api.libs.concurrent.Execution.Implicits._
 
-//    "send 404 on a bad request" in new WithApplication {
-//      route(FakeRequest(GET, "/boum")) must beNone
-//    }
-//
-//    "render the index page" in new WithApplication {
-//      val home = controllers.Application.index()(FakeRequest())
-//
-//      status(home) must equalTo(OK)
-//      contentType(home) must beSome.which(_ == "text/html")
-//      contentAsString(home) must contain ("Send emails")
-//    }
+  lazy val app = FakeApplication()
 
-    "Accept post data for sms" in new WithApplication {
+  override def around[T: AsResult](t: => T): Result = {
+    Helpers.running(app)(AsResult.effectively {
+      initDB(app)
+      try {
+        t
+      } finally {
+//        teardown(app)
+      }
+    })
+  }
 
-      val request = FakeRequest(POST, "/sms/").withFormUrlEncodedBody(
-        "To" -> "666666666",
-        "From" -> "77777777",
-        "Body" -> "hello toto"
-      )
+  def initDB(app: Application) {
+    implicit val application = app
+    val timeout = 20.seconds
+    val db: reactivemongo.api.DB = ReactiveMongoPlugin.db
 
-      val response = controllers.SmsService.receive()(request)
+    data.foreach {
+      case (collectionName, initialObjects, format) =>
+        Logger.warn(s"Initializing collection $collectionName with data $initialObjects")
+        val collection: JSONCollection = db.collection[JSONCollection](collectionName)
 
-      status(response) must equalTo(OK)
-      contentAsString(response) must contain ("Message")
+        // recover in case the collection does not exist
+        val dropFuture = collection.drop() recover {
+          case t @ _ =>
+            false
+        }
+        Await.result(dropFuture, timeout)
+
+        Logger.warn(s"Inserting data in DB")
+        implicit val implicitFormat = format
+//        val bsonObjects = initialObjects.map(obj => format.writes(obj))
+        val bsonObjects: List[BSONDocument] = initialObjects.map(obj => format.writes(obj).asInstanceOf[BSONDocument])
+
+        val insertFuture: Future[Int] = collection.bulkInsert(Enumerator.enumerate(bsonObjects)).mapTo[Int] recover {
+          case t @ _ =>
+            Logger.warn(s"Recovered $t")
+            0
+        }
+        Logger.warn(s"Inserting data in DB DONE")
+        val inserted = Await.result(insertFuture, timeout)
+
+        Logger.warn(s"Inserting data in DB DONE")
+        if (inserted < initialObjects.length) {
+          throw new Exception(s"Could not insert all initial data of collection $initialObjects: inserted $inserted instead of ${initialObjects.length}")
+        }
     }
 
-//    "render the sms list page" in new WithApplication {
-//      val response = controllers.SmsService.list()(FakeRequest())
+  }
+}
+
+
+
+@RunWith(classOf[JUnitRunner])
+class SmsSpec extends Specification with PlaySpecification {
+  sequential
+//  isolated
+
+  step {
+    Logger.info("Before class")
+  }
+
+  val smsData = ("smsList", List(Sms("11111111", "222222222", "some text")), JsonFormats.smsFormat)
+
+//  def setup(app: Application) {
+//    Logger.info("------------------------------------------INIT DATABASE")
+//    implicit val application = app
+//
+//    val db: reactivemongo.api.DB = ReactiveMongoPlugin.db
+//    val collection: JSONCollection = db.collection[JSONCollection]("smslist")
+//
+//    val dropFuture = collection.drop()
+//    await(dropFuture)
+//
+//    import JsonFormats._
+//    val sms = Sms("11111111", "222222222", "some text")
+//
+//    val insertFuture = collection.insert(sms).mapTo[LastError] map { lastError =>
+//      if (lastError.inError == true) {
+//        val message = s"Could not save the sms: ${lastError.message}"
+//        Logger.warn(message)
+//      } else {
+//        Logger.warn(s"Ok: $lastError")
+//      }
+//    }
+//
+//    await(insertFuture)
+//  }
+
+  "Sms module" should {
+
+//    "render the sms list page" in new WithEnv(setup = setup) {
+    "render the sms list page" in new InitDB(data=smsData) {
+
+      Logger.info("------------------------------------------Start test XXXXXXXXXXXXXXX")
+      val response = controllers.SmsService.list()(FakeRequest())
+
+      status(response) must equalTo(OK)
+      contentType(response) must beSome.which(_ == "text/html")
+      contentAsString(response) must contain ("List")
+
+      Logger.info(s"------------------------------------------${contentAsString(response)}")
+    }
+
+//    "Accept post data for sms" in new WithApplication with SmsInitDB {
+//      Logger.info("------------------------------------------Start test XXXXXXXXXXXXXXX")
+//
+//      val request = FakeRequest(POST, "/sms/").withFormUrlEncodedBody(
+//        "To" -> "666666666",
+//        "From" -> "77777777",
+//        "Body" -> "hello toto"
+//      )
+//
+//      val response = controllers.SmsService.receive()(request)
 //
 //      status(response) must equalTo(OK)
-//      contentType(response) must beSome.which(_ == "text/html")
-//      contentAsString(response) must contain ("List")
+//      contentAsString(response) must contain("Message")
+//
+//      import JsonFormats._
+//      val cursor: Cursor[Sms] = collection.
+//        // find all sms
+//        find(Json.obj()).
+//        // perform the query and get a cursor of JsObject
+//        cursor[Sms]
+//      val future = cursor.collect[List]()
+//      val result = await(future)
+//
+//      result should have size (1)
 //    }
-
 
   }
 
+  "Main module" should {
 
+    "send 404 on a bad request" in new WithApplication {
+      route(FakeRequest(GET, "/boum")) must beNone
+    }
+
+    "render the index page" in new WithApplication {
+      val home = controllers.Application.index()(FakeRequest())
+
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
+      contentAsString(home) must contain("Send emails")
+    }
+  }
+
+  step {
+    Logger.info("After class")
+  }
 }
