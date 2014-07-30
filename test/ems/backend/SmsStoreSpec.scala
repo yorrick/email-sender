344a8cb6ec77b2d0a851f51ce09bf1eb2a779877
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-07-30 16:08:54 -0400

    #15 added save test

--- test/ems/backend/SmsStoreSpec.scala
+++ test/ems/backend/SmsStoreSpec.scala
@@ -1,12 +1,14 @@
 package ems.backend
 
 
+import reactivemongo.bson.BSONObjectID
+import com.github.nscala_time.time.Imports._
 import org.junit.runner.RunWith
 import org.specs2.runner._
 import play.api.test._
 
 import ems.utils.{WithMongoData, WithMongoTestData}
-import reactivemongo.bson.BSONObjectID
+import ems.models.Sms
 
 
 @RunWith(classOf[JUnitRunner])
@@ -14,6 +16,14 @@ class SmsStoreSpec extends PlaySpecification with WithMongoTestData {
   sequential
 
   "Sms store" should {
+
+    "Be able to save a sms" in new WithMongoData(data) {
+      val smsId = SmsStore.generateId
+      val sms = smsList.head.copy(_id = smsId)
+      val result = await(SmsStore.save(sms))
+
+      result._id must beEqualTo(smsId)
+    }
 
     "List user sms" in new WithMongoData(data) {
       val objectId = BSONObjectID.parse(userMongoId).get
