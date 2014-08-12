b27a2071df29644161353d5f26b1876a5c05f369
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-08-12 14:04:21 -0400

    #20 forwarding display

--- app/ems/views/forwarding/updates.scala.js
+++ app/ems/views/forwarding/updates.scala.js
@@ -8,7 +8,7 @@ $(function() {
     var r = jsRoutes.ems.controllers.ForwardingController.updatesSocket();
     var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket
     var chatSocket = new WS(r.webSocketURL())
-    var elementTemplate = '@ems.views.html.forwarding.listElement(ForwardingDisplay(IdMapping.templateTag, UserIdMapping.templateTag, FromMapping.templateTag, ToMapping.templateTag, ContentMapping.templateTag, CreationMapping.templateTag, StatusCodeMapping.templateTag, StatusMapping.templateTag, SpinMapping.templateTag, SmsToEmailMapping.templateTag, EmailToSmsMapping.templateTag))'
+    var elementTemplate = '@ems.views.html.forwarding.listElement(ForwardingDisplay(IdMapping.templateTag, UserIdMapping.templateTag, FromMapping.templateTag, ToMapping.templateTag, ContentMapping.templateTag, CreationMapping.templateTag, StatusCodeMapping.templateTag, StatusMapping.templateTag, SmsToEmailMapping.templateTag, EmailToSmsMapping.templateTag))'
 
     // given forwarding <a> tag and received data, updates the spinners
     var updateSpinner = function(element, data) {
@@ -17,14 +17,13 @@ $(function() {
         var spinner = statusButton.find("i");
         spinner.removeClass();
 
-        if (data.@SpinMapping.jsonName == "true") {
+        if (data.@StatusCodeMapping.jsonName == "Sending") {
             spinner.addClass("fa fa-spinner fa-spin");
         }
     }
 
     // given forwarding <a> tag and received data, update the glyphicon
     var updateGlyphicon = function(element, data) {
-          // spinning classes
           var iconSpan = element.find("span.glyphicon");
 
           if (data.@SmsToEmailMapping.jsonName == "true") {
@@ -32,6 +31,13 @@ $(function() {
           }
           if (data.@EmailToSmsMapping.jsonName == "true") {
               iconSpan.addClass("glyphicon-envelope");
+          }
+    }
+
+    // given forwarding <a> tag and received data, update the grid offset
+    var updateGridClasses = function(element, data) {
+          if (data.@EmailToSmsMapping.jsonName == "true") {
+              element.addClass("col-md-offset-1");
           }
     }
 
@@ -66,7 +72,8 @@ $(function() {
             var replaced = replaced.replace("@CreationMapping.templateTag", data.@CreationMapping.jsonName);
             var replaced = replaced.replace("@StatusCodeMapping.templateTag", data.@StatusCodeMapping.jsonName);
             var replaced = replaced.replace("@StatusMapping.templateTag", data.@StatusMapping.jsonName);
-            var replaced = replaced.replace("@SpinMapping.templateTag", data.@SpinMapping.jsonName);
+            var replaced = replaced.replace("@SmsToEmailMapping.templateTag", data.@SmsToEmailMapping.jsonName);
+            var replaced = replaced.replace("@EmailToSmsMapping.templateTag", data.@EmailToSmsMapping.jsonName);
 
             var forwardingElement = $(replaced).hide().prependTo('#forwardingList')
 
@@ -78,6 +85,7 @@ $(function() {
 
         updateSpinner(element, data);
         updateGlyphicon(element, data);
+        updateGridClasses(element, data);
     }
 
     chatSocket.onmessage = receiveEvent
