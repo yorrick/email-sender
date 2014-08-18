6d67e14239d451d1d1aba17f965a4aea88c476f4
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-08-18 15:37:23 -0400

    #26 integrate securesocial and scaldi

--- app/ems/backend/EMSAuthenticatorService.scala
+++ app/ems/backend/EMSAuthenticatorService.scala
@@ -2,17 +2,21 @@ package ems.backend
 
 
 import scaldi.{Injector, Injectable}
-import securesocial.core.authenticator.{AuthenticatorStore, CookieAuthenticator, IdGenerator, CookieAuthenticatorBuilder}
+import securesocial.core.authenticator._
 import securesocial.core.services.AuthenticatorService
 
 import ems.models.User
 
 
 /**
- * Autenticator service for the project
+ * Authenticator service for the project
  * It only uses Cookies
  */
 class EMSAuthenticatorService(implicit inj: Injector) extends AuthenticatorService[User] with Injectable {
+
   val store = inject[AuthenticatorStore[CookieAuthenticator[User]]]
-  val builders = new CookieAuthenticatorBuilder[User](store, new IdGenerator.Default())
+  val builders = Seq(new CookieAuthenticatorBuilder[User](store, new IdGenerator.Default()))
+
+  override val asMap = builders.map { builder => builder.id -> builder }.toMap
+
 }
