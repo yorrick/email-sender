ec5739a21b17e5181e60d2d0b828165f2141fc7d
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-08-27 12:36:01 -0400

    #32 started to implement better form validation

--- app/ems/views/utils/Helpers.scala
+++ app/ems/views/utils/Helpers.scala
@@ -1,12 +1,7 @@
 package ems.views.utils
 
 
-import ems.views.html.utils.emsFieldConstructorTemplate
-import views.html.helper.FieldConstructor
-
-
 object Helpers {
-  implicit val emsFields = FieldConstructor(emsFieldConstructorTemplate.f)
 
   case class Alert(code: String, cssClass: String, iconName: String)
   object warning extends Alert("warning", "alert-danger", "glyphicon-warning-sign")
