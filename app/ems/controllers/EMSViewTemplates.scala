99926f6b22bbfabfc6c4fca25198b56e146d9ac2
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-09-02 19:18:19 -0400

    #27 inject context everywhere, part 7

--- app/ems/controllers/EMSViewTemplates.scala
+++ app/ems/controllers/EMSViewTemplates.scala
@@ -1,7 +1,6 @@
 package ems.controllers
 
 import ems.backend.cms.PrismicService
-import ems.controllers.utils.Context
 import play.api.mvc.RequestHeader
 import play.twirl.api.Html
 import play.api.data.Form
@@ -14,10 +13,11 @@ import securesocial.controllers.{ChangeInfo, RegistrationInfo, ViewTemplates}
 class EMSViewTemplates(env: RuntimeEnvironment[_], prismicService: PrismicService) extends ViewTemplates {
   implicit val implicitEnv = env
 
-  override def getLoginPage(form: Form[(String, String)],
-                            msg: Option[String] = None)(implicit request: RequestHeader, lang: Lang): Html = {
-    // find a way to fetch CMS documents
-    ems.views.html.auth.login(msg)(request, lang, env, Context(Map()))
+  /**
+   * This method is not used since login method in LoginController is overriden
+   */
+  override def getLoginPage(form: Form[(String, String)], msg: Option[String] = None)(implicit request: RequestHeader, lang: Lang): Html = {
+    Html("")
   }
 
   override def getSignUpPage(form: Form[RegistrationInfo], token: String)(implicit request: RequestHeader, lang: Lang): Html = {
